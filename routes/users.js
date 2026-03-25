88af8da33b949df2890357cf8e98d720d78687a5
Hayden Bindel <124805187+hbindel26@users.noreply.github.com>
2026-03-25 18:40:33 -0500

    edit users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+let vOne = 10;
+let vTwo = 15;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  if (vOne > vTwo) {
+    vOne = vOne - vTwo;
+  } else {
+    vTwo = vTwo - vOne;
+  }
+  res.send(`Values are: ${vOne} and ${vTwo}`);
 });
 
 module.exports = router;
